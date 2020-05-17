f66f3d36ea9e12cb12cec4abfe20a9ecabe28b65
Joseph Utulu <joseph@Josephs-MacBook-Pro.local>
2020-05-17 19:19:32 +0100

    changes

--- routes/about.js
+++ routes/about.js
@@ -13,6 +13,24 @@ router.get('/about_director', function(req, res, next) {
 router.get('/projects', function(req, res, next) {
   res.render('projects', {title: 'Projects'})
 });
+router.get('/p2fprojects', function(req, res, next) {
+  res.render('p2f', {title: 'P2F'})
+});
+router.get('/p2fabout', function(req, res, next) {
+  res.render('p2fAbout', {title: 'Who We Are'})
+});
+router.get('/service', function(req, res, next) {
+  res.render('service', {title: 'Who is the Service for?'})
+});
+router.get('/assessment', function(req, res, next) {
+  res.render('assessment', {title: 'Assessment'})
+});
+router.get('/diversity', function(req, res, next) {
+  res.render('diversity', {title: 'Diversity & Equality'})
+});
+router.get('/donation', function(req, res, next) {
+  res.render('donation', {title: 'Donation Statemet'})
+});
 
 router.get('/gallery', function(req, res, next) {
   res.render('gallery', {title: 'Gallery'})
